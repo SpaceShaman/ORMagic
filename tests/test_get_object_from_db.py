ec869dfc0be60fd1ea9c7b47ca90bb26df7d6b14
SpaceShaman <spaceshaman@tuta.io>
2024-07-20 22:58:14 +0200

    feat(test_get_object_from_db.py): refactor test_get_object_from_db.py to use a fixture to prepare the database before each test for better isolation and readability

--- tests/test_get_object_from_db.py
+++ tests/test_get_object_from_db.py
@@ -3,44 +3,35 @@ import pytest
 from pydantic_db.models import DBModel, NotExist
 
 
-def test_get_object_from_db(db_cursor):
-    class User(DBModel):
-        name: str
-        age: int
-
-    User.create_table()
+@pytest.fixture
+def prepare_db(db_cursor):
+    create_table_sql = "CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY, name TEXT NOT NULL, age INTEGER NOT NULL)"
+    db_cursor.execute(create_table_sql)
+    db_cursor.connection.commit()
     data = [("John", 30), ("Jane", 25), ("Doe", 35)]
     db_cursor.executemany("INSERT INTO user (name, age) VALUES (?, ?)", data)
     db_cursor.connection.commit()
 
+
+class User(DBModel):
+    name: str
+    age: int
+
+
+def test_get_object_from_db(prepare_db):
     user = User.get(id=2)
 
     assert user.name == "Jane"
     assert user.age == 25
 
 
-def test_get_object_from_db_by_name(db_cursor):
-    class User(DBModel):
-        name: str
-        age: int
-
-    User.create_table()
-    data = [("John", 30), ("Jane", 25), ("Doe", 35)]
-    db_cursor.executemany("INSERT INTO user (name, age) VALUES (?, ?)", data)
-    db_cursor.connection.commit()
-
+def test_get_object_from_db_by_name(prepare_db):
     user = User.get(name="Doe")
 
     assert user.name == "Doe"
     assert user.age == 35
 
 
-def test_try_to_get_non_existing_object_from_db(db_cursor):
-    class User(DBModel):
-        name: str
-        age: int
-
-    User.create_table()
-
+def test_try_to_get_non_existing_object_from_db(prepare_db):
     with pytest.raises(NotExist):
-        User.get(id=1)
+        User.get(id=100)
