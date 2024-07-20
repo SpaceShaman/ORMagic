f271e2faf43aa0b3627314e3abc9ea8435a88d30
SpaceShaman <spaceshaman@tuta.io>
2024-07-20 23:00:52 +0200

    test(test_save_model_in_db.py): refactor test to save multiple user records in the database for better coverage and testing efficiency

--- tests/test_save_model_in_db.py
+++ tests/test_save_model_in_db.py
@@ -8,9 +8,10 @@ def test_save_data_to_db(db_cursor):
 
     User.create_table()
 
-    user = User(name="John", age=30)
-    user.save()
+    User(name="John", age=30).save()
+    User(name="Jane", age=25).save()
+    User(name="Doe", age=35).save()
 
     res = db_cursor.execute("SELECT * FROM user")
     data = res.fetchall()
-    assert data == [(1, "John", 30)]
+    assert data == [(1, "John", 30), (2, "Jane", 25), (3, "Doe", 35)]
