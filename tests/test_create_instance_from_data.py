253e0a318d9540ce4564119fdfcd610ae4c235f8
SpaceShaman <spaceshaman@tuta.io>
2024-07-23 23:07:34 +0200

    test(test_create_instance_from_data.py): add test case for creating an instance with a foreign key relationship to ensure correct data retrieval and association between models

--- tests/test_create_instance_from_data.py
+++ tests/test_create_instance_from_data.py
@@ -11,3 +11,25 @@ def test_create_instance_from_data():
     assert user.id == 1
     assert user.name == "John"
     assert user.age == 25
+
+
+def test_create_instance_from_data_with_foreign_key():
+    class User(DBModel):
+        name: str
+        age: int
+
+    class Post(DBModel):
+        title: str
+        user: User
+
+    User.create_table()
+    Post.create_table()
+    User(name="John", age=25).save()
+
+    post = Post._create_instance_from_data((1, "First post", 1))
+
+    assert post.id == 1
+    assert post.title == "First post"
+    assert post.user.id == 1
+    assert post.user.name == "John"
+    assert post.user.age == 25
