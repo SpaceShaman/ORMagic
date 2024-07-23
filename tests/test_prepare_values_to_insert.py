b81be312ec856115fd9efe7c4af278dc1b50de87
SpaceShaman <spaceshaman@tuta.io>
2024-07-23 23:14:13 +0200

    chore(tests): delete test_.py file as it is no longer needed feat(tests): add new test file test_prepare_values_to_insert.py to test the _prepare_values_to_insert method in DBModel class

---
+++ tests/test_prepare_values_to_insert.py
@@ -0,0 +1,33 @@
+from ormagic.models import DBModel
+
+
+def test_prepare_values_to_insert():
+    class User(DBModel):
+        name: str
+        age: int
+
+    user = User(name="John", age=25)
+
+    values = user._prepare_values_to_insert({"name": "John", "age": 25})
+
+    assert values == "'John', '25'"
+
+
+def test_prepare_values_to_insert_with_foreign_key():
+    class User(DBModel):
+        name: str
+        age: int
+
+    class Post(DBModel):
+        title: str
+        user: User
+
+    User.create_table()
+    Post.create_table()
+    User(name="John", age=25).save()
+
+    post = Post(title="First post", user=User.get(id=1))
+
+    values = post._prepare_values_to_insert({"title": "First post", "user": {"id": 1}})
+
+    assert values == "'First post', '1'"
