cf665e762f1b8b60775a93c9396e8906d8aef5b0
SpaceShaman <spaceshaman@tuta.io>
2024-08-06 19:35:13 +0200

    ♻️ (data_saver.py, models.py): refactor _insert method to data_saver.py for better separation of concerns

--- ormagic/data_saver.py
+++ ormagic/data_saver.py
@@ -1,2 +1,19 @@
+from .sql_utils import execute_sql
+
+
 def save(self):
-    return self._update() if self.id else self._insert()
+    return self._update() if self.id else _insert(self)
+
+
+def _insert(self):
+    prepared_data = self._prepare_data_to_insert(self.model_dump(exclude={"id"}))
+    fields = ", ".join(prepared_data.keys())
+    values = ", ".join(
+        f"'{value}'" if value else "NULL" for value in prepared_data.values()
+    )
+    sql = f"INSERT INTO {self._get_table_name()} ({fields}) VALUES ({values})"
+    cursor = execute_sql(sql)
+    cursor.connection.close()
+    self.id = cursor.lastrowid
+    self._update_many_to_many_intermediate_table()
+    return self
--- ormagic/models.py
+++ ormagic/models.py
@@ -55,19 +55,6 @@ class DBModel(BaseModel):
         if cursor.rowcount == 0:
             raise ObjectNotFound
 
-    def _insert(self) -> Self:
-        prepared_data = self._prepare_data_to_insert(self.model_dump(exclude={"id"}))
-        fields = ", ".join(prepared_data.keys())
-        values = ", ".join(
-            f"'{value}'" if value else "NULL" for value in prepared_data.values()
-        )
-        sql = f"INSERT INTO {self._get_table_name()} ({fields}) VALUES ({values})"
-        cursor = execute_sql(sql)
-        cursor.connection.close()
-        self.id = cursor.lastrowid
-        self._update_many_to_many_intermediate_table()
-        return self
-
     def _update(self) -> Self:
         prepared_data = self._prepare_data_to_insert(self.model_dump(exclude={"id"}))
         fields = ", ".join(
