c2848e6656f999cc668f725007614893c13a981f
SpaceShaman <spaceshaman@tuta.io>
2025-01-11 14:19:09 +0100

    ✅ (tests): update test_simple_keyword_argument_filters for database compatibility
    ♻️ (tests): refactor test to handle different db types in test_simple_keyword_argument_filters
    
    🔧 (tests): add db.sqlite3-shm and db.sqlite3-wal for database state management

--- tests/test_simple_keyword_argument_filters.py
+++ tests/test_simple_keyword_argument_filters.py
@@ -1,18 +1,24 @@
-from sqlite3 import OperationalError
-
 import pytest
 
 from ormagic.models import DBModel
+from ormagic.settings import Settings
 
 
 @pytest.fixture
 def prepare_db(cursor):
-    cursor.execute(
-        "CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY, name TEXT NOT NULL, age INTEGER NOT NULL)"
-    )
-    cursor.connection.commit()
     data = [("John", 30), ("Jane", 25), ("Doe", 35), ("John", 40)]
-    cursor.executemany("INSERT INTO user (name, age) VALUES (?, ?)", data)
+    if Settings().db_type == "postgresql":
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT NOT NULL, age INTEGER NOT NULL)"
+        )
+        cursor.connection.commit()
+        cursor.executemany("INSERT INTO users (name, age) VALUES (%s, %s)", data)
+    else:
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT NOT NULL, age INTEGER NOT NULL)"
+        )
+        cursor.connection.commit()
+        cursor.executemany("INSERT INTO users (name, age) VALUES (?, ?)", data)
     cursor.connection.commit()
 
 
@@ -51,7 +57,7 @@ def test_filter_objects_with_no_results(prepare_db, cursor):
 
 
 def test_try_to_filter_objects_with_invalid_field(prepare_db, cursor):
-    with pytest.raises(OperationalError):
+    with pytest.raises(Exception):
         User.filter(invalid_field="Jane")
 
 
